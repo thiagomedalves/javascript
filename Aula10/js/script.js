91c706ace82b48eac8b60539577526268ebe56fe
THIAGO MEDEIROS ALVES <thiago.malves5@senacsp.edu.br>
2024-06-29 09:52:09 -0300

    continuação Aula 10 , estilização do banner

--- Aula10/js/script.js
+++ Aula10/js/script.js
@@ -26,18 +26,71 @@ fetch("https://api.themoviedb.org/3/movie/popular?language=pt-br&page=1&api_key=
         let cartaz = document.createElement("div");
         cartaz.setAttribute("class", "cartaz");
         let poster = document.createElement("img");
-        poster.src= img+filme.poster_path;
+       poster.src= img+filme.poster_path;
        let votos = document.createElement("div");
        votos.setAttribute("class","votos");
-
-       votos.innerHTML = filme.vote_average;
+       votos.innerHTML = Math.round (filme.vote_average)+"%";
 
        let titulo = document.createElement("h2");
        titulo.innerHTML = filme.title;
 
         let lancamento = document.createElement("p");
         lancamento.setAttribute("class","lancamento");
-        lancamento.innerHTML=filme.realise_date;
+        /* vamos utilizar o comando chamado substring , que 
+        nos ajuda a quebrer uma string com base na posiçao do caracter 
+        e a quantidade de quantidades 
+        por exemplo: 2024-06-13. para pegar os 4 primeiros digitos 
+        usamos : 
+        substring(0,4).
+        */
+        
+        let ano = filme.release_date.substring(0,4);
+        let mes = filme.release_date.substring(5,7);
+        let dia = filme.release_date.substring(8,10);
+      
+        switch(mes){
+            case "01":
+            mes = "jan";
+                break;
+            case "02":
+            mes = "fev"
+                break;
+            case "03":
+                mes = "mar"
+                break;
+            case "04":
+                mes = "abri"
+                break
+            case "05":
+                mes = "mai"
+                break
+            case "06":
+                mes = "jun"
+                break
+            case "07":
+                mes = "jul"
+                break
+            case "08":
+                mes = "ago"
+                break
+            case "09":
+                mes = "set"
+                break
+            case "10":
+                mes = "out"
+                break
+            case "11":
+                mes = "nov"
+                break
+            case "12":
+                mes = "dez"
+                break
+            
+        }
+
+
+        
+        lancamento.innerHTML=`${dia} de ${mes} de ${ano}` ;
 
        
        
